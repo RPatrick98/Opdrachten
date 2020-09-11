9acd5cc95187e7c389c7a1e08eb3c4193f4bb345
RPatryk98 <99049638@mydavinci.nl>
2020-09-11 08:49:17 +0200

    L opdrachten van js

---
+++ b1w2l1.js
@@ -0,0 +1,13 @@
+/*
+Patryk Rachanczyk
+Software Development
+*/
+
+alert('Beantwoord 3 vragen');
+var name; // variable name 
+var surname;  // variable surname 
+var age; // variable age
+name = prompt("Hoe heet je?"); // er laat gebruiker window zien om naam in te voeren die bij variable name hoort
+surname = prompt("Wat is jou achternaam?"); // er laat gebruiker window zien om achternaam in te voeren die bij variable surname hoort
+age = prompt("Hoe oud ben je?");  // er laat gebruiker window zien om leeftijd in te voeren die bij variable age hoort
+document.write(name + "<br>" + surname + "<br>" + age); // er schrijft op het scherm alle drie opgeslaagde waarden in de variable die met 3 regels geselecteerd zijn
---
+++ b1w2l2.js
@@ -0,0 +1,12 @@
+/*
+Patryk Rachanczyk
+Software Development
+*/
+
+var name; // variable name 
+var surname;  // variable surname 
+var age; // variable age
+name = prompt("Hoe heet je?"); // er laat gebruiker window zien om naam in te voeren die bij variable name hoort
+surname = prompt("Wat is jou achternaam?"); // er laat gebruiker window zien om achternaam in te voeren die bij variable surname hoort
+age = prompt("Hoe oud ben je?");  // er laat gebruiker window zien om leeftijd in te voeren die bij variable age hoort
+document.write(name + "<br>" + surname + "<br>" + age); // er schrijft alle drie opgeslaagde waarden in de variable op het scherm die met 3 regels geselecteerd zijn
